ce1c8e176240b39605130734e139e424735e8bf4
Andrew Mercer <mercerraa@gmail.com>
2025-02-05 10:57:06 +0100

    Update dataExport.py
    Functional script that works either by usig predefined list of län names or by searching for unique values.
    Second option better for "byggnader" which have poorly formatted län names

--- scripts/dataExport.py
+++ scripts/dataExport.py
@@ -4,32 +4,19 @@
 # Concatenates each export into one file
 # Intended use: Bebyggelseregistret
 #  
+import os
 from qgis.core import Qgis
 from qgis.core import QgsProject
 from qgis.utils import iface
 
 def getActive():
-    '''Sets the active layer as variable name, e.g. layer1 = getActive()'''
+    '''Gets the active layer and retuns pyqgis object layer'''
     layer = qgis.utils.iface.activeLayer()
     print(f'Layer ID: {layer.id()}')
     print(f'Feature count: {layer.featureCount()}')
     for field in layer.fields():
         print(field.name(), field.type())
     return layer
-
-def selectFilterExpWord(layer, column, word, iter = False, shell = False):
-    '''select objects from layer by searching for a string in an attribute column. Returns either a list of objects or an iterator'''
-    selection = []
-    request = QgsFeatureRequest().setFilterExpression(' "{}" = \'{}\' '.format(column, str(word)))
-    iterator = layer.getFeatures(request)
-    if iter == True: 
-        return iterator
-    else: 
-        for found in iterator:
-            if shell == True: print(found)
-            selection.append(found)
-            layer1.select(found.id())
-        return selection
 
 def addVardef(filename, line):
     '''Prepends variable definition to head of file, making geoJson object an importable variable'''
@@ -39,14 +26,33 @@ def addVardef(filename, line):
         f.write(line.rstrip('\r\n') + '\n' + content)
     return
 
-def exportGeojson(layer, filename, porp, lan):
+def exportGeojson(layer, field, value, path, geometry):
     '''Exports selected objects to GeoJson and then calls addVardef to prepend variable definition'''
+    selectText = f"\"{field}\" ilike \'%{value}%\'"
+    print(selectText)
+    layer.selectByExpression(selectText)
+    name = geometry + value
+    filename = os.path.join(path, name)
     QgsVectorFileWriter.writeAsVectorFormat(layer, f'{filename}', 'utf-8', QgsCoordinateReferenceSystem('EPSG:4326'), 'GeoJSON', layerOptions=['COORDINATE_PRECISION=8'], onlySelected=True)
-    line = f"let {porp}{lan} = "
-    addVardef(filename, line)
+    line = f"let {geometry}{value} = "
+    addVardef(filename + '.geojson', line)
+    print('Done.')
+    return
 
-porp = "point"
-# porp = "polygon"
+def exportUniqueGeojson(layer, field, path, geometry):
+    idx = layer.fields().indexOf(field)
+    unique_values = layer.uniqueValues(idx)
+    print(f'Found values: {unique_values}')
+    for value in unique_values:
+        layer.selectByExpression(f"{field}='{value}'")
+        name = geometry + value
+        filename = os.path.join(path, name)
+        QgsVectorFileWriter.writeAsVectorFormat(layer, f'{filename}', 'utf-8', QgsCoordinateReferenceSystem('EPSG:4326'), 'GeoJSON', layerOptions=['COORDINATE_PRECISION=8'], onlySelected=True)
+        line = f"let {geometry}{value} = "
+        addVardef(filename + '.geojson', line)
+    print('Done.')
+    return
+
 lanList = [
     "Blekinge", "Dalarna", "Gotland",
     "Gävleborg", "Halland", "Jämtland",
@@ -56,6 +62,19 @@ lanList = [
     "Västerbotten", "Västernorrland", "Västmanland",
     "Västra Götaland", "Örebro", "Östergötland"]
 
-for lan in lanList:
-    
+path = QgsProject.instance().readPath("./")
+os.chdir(path)
 
+layer = getActive()
+if layer.geometryType() == QgsWkbTypes.PointGeometry:
+    geometry = "point"
+elif layer.geometryType() == QgsWkbTypes.PolygonGeometry:
+    geometry = "polygon"
+elif layer.geometryType() == QgsWkbTypes.LineGeometry:
+    geometry = "line"
+else :
+    geometry = ""
+
+#exportUniqueGeojson(layer, 'lan', path, geometry)
+
+for lan in lanList: exportGeojson(layer, 'lan', lan, path, geometry)
